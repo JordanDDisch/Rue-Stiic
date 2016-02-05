7819a10877b244c0807a0af1f5c745b18a1c809a
Elijah Disch <elijahdisch@gmail.com>
2016-02-05 18:56:51 +1000

    Added kss-isolate and removed styleguide component

--- scss/components/kss-isolate/kss-isolate.js
+++ scss/components/kss-isolate/kss-isolate.js
@@ -1,23 +1,3 @@
-jQuery(function ($) {
-    var sf, body;
-    var breakpoint = 1180;
-
-    $(document).ready(function ($) {
-
-        $('.hamburger-menu__icon').click(function() {
-            $(".content-wrapper").toggleClass("content-wrapper--active");
-            $(".hamburger-menu__icon-container").toggleClass("content-wrapper--active");
-            $(".hamburger-menu").toggleClass("hamburger-menu--active");
-        });
-
-        $('.hamburger-menu ul li').click(function(event) {
-            if($(this).find('ul').hasClass('hamburger-menu__list--active') == false) {
-                event.preventDefault();
-                $(this).find('ul').addClass('hamburger-menu__list--active');
-            }
-        });
-    })
-});
 jQuery(function($) {
 
     function hideComponents(selected, button) {
